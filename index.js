b40ea2ec7a054b0e15b233e0d2e54581c5758c67
IGustiNgurahAgungSastraSidhiHartam <agungsastra0904@gmail.com>
2024-08-05 20:06:46 +0800

    feat(recipe-page)
    fix spas in instruction

--- index.js
+++ index.js
@@ -43,31 +43,31 @@ let Instructions = [
   {
     instruction: "Beat the eggs",
     description:
-      "In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.",
+      " In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.",
   },
   {
     instruction: "Heat the pan",
     description:
-      "Place a non-stick frying pan over medium heat and add butter or oil.",
+      " Place a non-stick frying pan over medium heat and add butter or oil.",
   },
   {
     instruction: "Cook the omelette",
     description:
-      "Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.",
+      " Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.",
   },
   {
     instruction: "Add fillings (optional)",
     description:
-      "When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.",
+      " When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.",
   },
   {
     instruction: "Fold and serve",
     description:
-      "As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.",
+      " As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate.",
   },
   {
     instruction: "Enjoy",
-    description: "Serve hot, with additional salt and pepper if needed.",
+    description: " Serve hot, with additional salt and pepper if needed.",
   },
 ];
 instructionList.innerHTML = Instructions.map((instruction) => {
